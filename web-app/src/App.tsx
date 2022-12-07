a7ad867344d919be78e12e22858a83c71738923a
Vicky Lee <vicky.winkay.lee@gmail.com>
2022-12-07 11:34:18 +0000

    add basic interactive world map

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactTooltip from "react-tooltip";
+import WorldMap from "./WorldMap";
+import "./map.css";
 
-function App() {
+export default function App() {
+  const [content, setContent] = React.useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <WorldMap setTooltipContent={setContent} />
+      <ReactTooltip>{content}</ReactTooltip>
     </div>
   );
 }
-
-export default App;
