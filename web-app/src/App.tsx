bbd1a8cf65315d1be20ef95e0184225c0869cdef
Vicky Lee <vicky.winkay.lee@gmail.com>
2022-12-09 00:05:11 +0000

    add basic fe

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -3,34 +3,73 @@ import ReactTooltip from "react-tooltip";
 import WorldMap from "./WorldMap";
 import "./map.css";
 import { Button, TextField } from '@mui/material';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from "dayjs";
+
+const defaultDestinations = `
+AUS,JFK,
+BCN,LIS,LHR,
+TYO,BKK
+`.trim()
 
 export default function App() {
   const [content, setContent] = React.useState("");
   const [origins, setOrigins] = React.useState("")
   const [budget, setBudget] = React.useState(0)
-  const [destination, setDestination] = React.useState("")
+  const [departureDate, setDepartureDate] = React.useState<any>("")
+  const [returnDate, setReturnDate] = React.useState<any>("")
+  const [destinations, setDestinations] = React.useState(defaultDestinations)
 
-  const formatUrl = () => {
+  const formatUrl = (destination: string) => {
     const formattedOrigins = origins.split(",").map(origin => `&origins=${origin}`).join("")
-    return `${process.env.REACT_APP_API_URL}?destination=OPO&departureDate=2023-05-15&returnDate=2023-05-19${formattedOrigins}`
+    const formattedDeparture = dayjs(departureDate).format("YYYY-MM-DD")
+    const formattedReturn = dayjs(returnDate).format("YYYY-MM-DD")
+
+    return `${process.env.REACT_APP_API_URL}?destination=${destination}&departureDate=${formattedDeparture}&returnDate=${formattedReturn}${formattedOrigins}`
   }
 
   const handleSearch = () => {
-    console.log(formatUrl())
-    alert(formatUrl())
+    destinations.split(",").map(d => console.log(formatUrl(d)))
   }
 
   return (
     <div>
-       <TextField
-          id="origin-airport-codes"
-          label="Origin airport codes"
-          multiline
-          rows={6}
-          defaultValue=""
-          variant="filled"
-          onChange={(e) => setOrigins(e.target.value as any)}
+      <TextField
+        id="origin-airport-codes"
+        label="Origin airport codes"
+        multiline
+        rows={6}
+        defaultValue={origins}
+        variant="filled"
+        onChange={(e) => setOrigins(e.target.value as any)}
+      />
+      <TextField
+        id="destination-airport-codes"
+        label="Destination airport codes"
+        multiline
+        rows={6}
+        defaultValue={destinations}
+        variant="filled"
+        onChange={(e) => setDestinations(e.target.value as any)}
+      />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DesktopDatePicker
+          label="Departure date"
+          inputFormat="YYYY-MM-DD"
+          value={departureDate}
+          onChange={setDepartureDate}
+          renderInput={(params) => <TextField {...params} />}
         />
+        <DesktopDatePicker
+          label="Return date"
+          inputFormat="YYYY-MM-DD"
+          value={returnDate}
+          onChange={setReturnDate}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
       <TextField
         id="budget"
         label="Budget"
